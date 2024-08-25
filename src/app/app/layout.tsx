41760a2574cfa4dfd291e8e2f82e5bf4714e4214
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-08-24 21:19:43 -0300

    feat: ajuste layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,6 +1,11 @@
-import { PropsWithChildren, useEffect } from "react";
-
+import { PropsWithChildren} from "react";
+import { MainSidebar } from "./_components/main-sidebar";
 
 export default function Layout({ children }: PropsWithChildren) {
-  return <>{children}</>;
+  return (
+    <div className="grid grid-cols-[16rem_1fr]">
+      <MainSidebar />
+      <main>{children}</main>
+    </div>
+  );
 }
