bb0efca03e8b4dc1302ccb56b2fd5ad7a15d9c4d
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-08-28 01:15:54 -0300

    feat: ajuste nas pages

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -1,7 +1,9 @@
-'use client'
-import { SignOutButton, UserButton } from "@clerk/nextjs";
+"use client";
+import { Button } from "@/components/ui/button";
 import { useAuth } from "@clerk/nextjs";
+import { PlusIcon } from "@radix-ui/react-icons";
 import { useEffect } from "react";
+import { ProductTable } from "../_components/product-tablet";
 export default function Page() {
   const { isSignedIn } = useAuth();
 
@@ -14,9 +16,25 @@ export default function Page() {
   }, [isSignedIn]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>Dashboard</h1>
-      <SignOutButton redirectUrl="/auth/sign-in" />
-    </div>
-  )
+    <>
+      <div className="scrollContainer h-[calc(100vh-60px)] overflow-auto pb-10">
+        <div className="justify-center items-center ">
+          <div className="flex items-center justify-between mx-auto max-w-5xl px-6 py-8">
+            <h3 className="text-[28px] leading-[34px] tracking-[-0.416px] text-slate-12 font-bold">
+              Lista de Compras
+            </h3>
+
+            <Button className="bg-[#7450AC] hover:bg-[#7450AC]">
+              <PlusIcon className="w-4 h-4 mr-3" />
+              Adicionar produto
+            </Button>
+          </div>
+
+          <div className="mx-auto max-w-5xl px-6">
+            <ProductTable />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/app/expiration/(main)/page.tsx
+++ src/app/app/expiration/(main)/page.tsx
@@ -1,7 +1,15 @@
 export default async function Page() { 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>produtos para vencer</h1>
-    </div>
+    <>
+      <div className="scrollContainer h-[calc(100vh-60px)] overflow-auto pb-10">
+        <div className="justify-center items-center ">
+          <div className="flex items-center justify-between mx-auto max-w-5xl px-6 py-8">
+            <h3 className="text-[28px] leading-[34px] tracking-[-0.416px] text-slate-12 font-bold">
+            produtos para vencer
+            </h3>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
