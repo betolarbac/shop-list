3e48c8d1cf9c5ebbe0a46cf4ad1812e1857a3336
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-08-30 01:41:17 -0300

    feat:  skeleton de usuario

--- src/app/app/_components/user-info.tsx
+++ src/app/app/_components/user-info.tsx
@@ -1,23 +1,39 @@
 import { useUser } from "@clerk/nextjs";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function UserInfo() {
-  const { user } = useUser();
-
+  const { user, isLoaded } = useUser();
 
   return (
-    <div className="flex gap-3 items-center">
-      <div>
-        <Avatar className="w-8 h-8">
-          <AvatarImage src={user?.imageUrl} alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </div>
+    <>
+      {isLoaded ? (
+        <div className="flex gap-3 items-center">
+          <div>
+            <Avatar className="w-8 h-8">
+              <AvatarImage src={user?.imageUrl} alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
 
-      <div>
-        <h2 className="text-base capitalize leading-5 line-clamp-1">{user?.fullName}</h2>
-        <p className="text-sm line-clamp-1">{user?.emailAddresses[0]?.emailAddress}</p>
-      </div>
-    </div>
+          <div>
+            <h2 className="text-base capitalize leading-5 line-clamp-1">
+              {user?.fullName}
+            </h2>
+            <p className="text-sm line-clamp-1">
+              {user?.emailAddresses[0]?.emailAddress}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Skeleton className="h-8 w-8 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[152px]" />
+            <Skeleton className="h-4 w-[152px]" />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
