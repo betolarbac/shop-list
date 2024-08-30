0d529139b7e32a3646c4f27e5098b50f5b9bb6d0
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-08-30 00:53:32 -0300

    feat:  action e schema de adicionar produtos

--- src/app/app/(main)/actions.ts
+++ src/app/app/(main)/actions.ts
@@ -2,6 +2,8 @@
 
 import { prisma } from "@/services/database/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { z } from "zod";
+import { upsertProductSchema } from "./schema";
 
 export async function getProducts() {
   const { userId }: { userId: string | null } = auth();
@@ -17,4 +19,29 @@ export async function getProducts() {
   });
 
   return products;
+}
+
+export async function upsertProduct(input: z.infer<typeof upsertProductSchema>) {
+  const { userId } = auth();
+  
+  if (!userId) {
+    throw new Error("Usuário não autenticado");
+  }
+  
+  const product = await prisma.products.upsert({
+    where: { id: input.id ?? '' },
+    update: {
+      title: input.title,
+      amount: input.amount,
+      value: input.value,
+    },
+    create: {
+      title: input.title || '',
+      amount: input.amount,
+      value: input.value,
+      userId,
+    }
+  });
+
+  return product;
 }
--- src/app/app/(main)/schema.ts
+++ src/app/app/(main)/schema.ts
@@ -3,6 +3,6 @@ import {z } from "zod"
 export const upsertProductSchema = z.object({
   id: z.string().optional(),
   title: z.string().optional(),
-  amount: z.string().optional(),
-  value: z.string().optional(),
+  amount: z.union([z.string().transform((val) => parseFloat(val)), z.number()]),
+  value: z.union([z.string().transform((val) => parseFloat(val)), z.number()]),
 })
