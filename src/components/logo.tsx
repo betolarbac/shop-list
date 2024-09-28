12a38cd630b122942423e90a84c0c84d31a19666
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-09-28 19:50:14 -0300

    feat: ajuste de cor da logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,10 @@
-import { ShoppingCart, ShoppingBag } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 
 export function Logo() {
   return (
     <div className="flex gap-2 ">
       <div className="bg-slate-12 h-6 w-6 flex items-center justify-center rounded-md p-1">
-        <ShoppingCart className="w-[18px] h-[18px] text-primary-foreground text-black" />
+        <ShoppingCart className="w-[18px] h-[18px]  text-black" />
       </div>
       <h4>ShopList</h4>
     </div>
