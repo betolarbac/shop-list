ca71effcc127598530fb1d41995d38f5c8f55466
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-12-01 23:40:54 -0300

    feat: atualização do nome de perfil

--- src/app/app/profile/[[...user-profile]]/page.tsx
+++ src/app/app/profile/[[...user-profile]]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <div className="justify-center items-center ">
         <div className="flex items-center justify-between mx-auto max-w-5xl px-6 py-8">
           <h3 className="text-[16px] leading-[20px] tracking-[-0.416px] text-slate-12 font-bold lg:text-[28px] lg:leading-[34px]">
-            Produtos para vencer
+            Perfil
           </h3>
         </div>
 
